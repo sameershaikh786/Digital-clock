2b47be47f2a4e94fe23672187041ec9e26a21028
Sameer Shaikh <sshaikh99201@gmail.com>
2022-09-11 21:59:09 +0530

    second commit

--- main.java
+++ main.java
@@ -2,5 +2,20 @@ public class main {
 
   public static void main(String[] args) {
     window w1 = new window();
+    int x;
+    int y;
+    System.out.println(myFirst(2, 3));
   }
+  
+   static int myFirst(int x , int y){
+    x=45;
+    y=78;
+    int z= x+y;
+
+    return z;
+   }
+
+
+
+
 }
